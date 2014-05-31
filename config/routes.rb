6bafaaa28497fe82ba4ac54783573653c7c6f697
Carl Consing <cmconsing@gmail.com>
2014-05-31 12:50:05 -0700

    implemented devise for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Cproject::Application.routes.draw do
+  devise_for :users
   root "pages#home"
   get "messages" => "pages#messages"
   # The priority is based upon order of creation: first created -> highest priority.
