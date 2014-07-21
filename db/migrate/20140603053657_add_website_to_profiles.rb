ac21319ba0bdc1828ed0c368a10c7910f302db18
Carl Consing <cmconsing@gmail.com>
2014-07-21 01:06:25 -0700

    added looking_for and stockists fields to registration form

--- db/migrate/20140603053657_add_website_to_profiles.rb
+++ db/migrate/20140603053657_add_website_to_profiles.rb
@@ -7,5 +7,7 @@ class AddWebsiteToProfiles < ActiveRecord::Migration
     add_column :profiles, :brands_we_carry, :string
     add_column :profiles, :description, :string
     add_column :profiles, :is_brand, :boolean, default: true
+    add_column :profiles, :looking_for, :string
+    add_column :profiles, :stockist, :string
   end
 end
