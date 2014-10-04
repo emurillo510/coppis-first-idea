524029222f7831831e20e4e8cc8a19f06222f751
Carl Consing <cmconsing@gmail.com>
2014-10-03 21:34:11 -0700

    reset migrations

--- db/migrate/20140601075235_add_name_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddNameToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :name, :string
-    add_column :users, :website, :string
-    add_column :users, :is_brand, :boolean, default: false
-
-  end
-end
--- db/migrate/20140825041016_add_profile_id_to_photos.rb
+++
@@ -1,6 +0,0 @@
-class AddProfileIdToPhotos < ActiveRecord::Migration
-  def change
-    add_column :photos, :profile_id, :integer
-    add_index :photos, :profile_id
-  end
-end
