80725ad6d44f3dbfd25d3858c630c43af28ae4c0
Menandro <mmadlansacay@gmail.com>
2014-10-05 06:11:16 -0700

    styled edit photo gallery page

--- config/routes.rb
+++ config/routes.rb
@@ -18,11 +18,9 @@ Cproject::Application.routes.draw do
   get "news" => "pages#news"
   get "brands" => "brands#index"
   get "retailers" => "retailers#index"
+  get "photos" => "photos#index"
   get 'tags/:tag', to: 'profiles#index', as: :tag
   get 'stockists/:stockists', to: 'profiles#index', as: :stockist
-  #get 'overview' => 'profiles#get_overview'
-  #get 'get_news' => 'profiles#get_news'
-  #get 'get_followers' => 'profiles#get_followers'
   get 'profiles/:id/overview' => 'profiles#get_overview', as: 'profile_get_overview'
   get 'profiles/:id/get-news' =>  'profiles#get_news', as: 'profile_get_news' 
   get 'profiles/:id/get-followers' =>  'profiles#get_followers' , as: 'profile_get_followers' 
