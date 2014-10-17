99b51ab1635d09b4553b5e98cd6326c349fdcd1f
Carl Consing <cmconsing@gmail.com>
2014-10-16 23:01:20 -0700

    fixed cover photo to crop and fit

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,7 +4,7 @@ class Profile < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
 	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
-  has_attached_file :cover, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :cover, :styles => { :medium => "300x300#", :thumb => "100x100#" },  :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :cover, :content_type => /\Aimage\/.*\Z/
 	has_many :relationships, foreign_key: "follower_id", dependent: :destroy
         has_many :followed_profiles, through: :relationships, source: :followed
