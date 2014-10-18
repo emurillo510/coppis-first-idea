76ea8a7ba12c93d4b58820482c5b6c5595d41fa0
Carl Consing <cmconsing@gmail.com>
2014-10-18 12:16:54 -0700

    display brands and retailers in alphabetical order

--- app/controllers/retailers_controller.rb
+++ app/controllers/retailers_controller.rb
@@ -1,13 +1,13 @@
 class RetailersController < ApplicationController
    def index
-      @profiles = Profile.where(:is_brand => false).paginate(:page => params[:page], :per_page =>8)
+      @profiles = Profile.all.order(:name).where(:is_brand => false).paginate(:page => params[:page], :per_page =>8)
    end
 
     def featured
-       @profiles = Profile.where({:is_brand => false , :featured => true}).paginate(:page => params[:page], :per_page =>8)
+       @profiles = Profile.all.order(:name).where({:is_brand => false , :featured => true}).paginate(:page => params[:page], :per_page =>8)
     end
 
     def trending
-       @profiles = Profile.where({:is_brand => false , :trending => true}).paginate(:page => params[:page], :per_page =>8)
+       @profiles = Profile.all.order(:name).where({:is_brand => false , :trending => true}).paginate(:page => params[:page], :per_page =>8)
     end
 end
