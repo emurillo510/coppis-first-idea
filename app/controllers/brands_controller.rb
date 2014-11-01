bfc5654cf0609b36275279726b8c258b68bf7752
Menandro <mmadlansacay@gmail.com>
2014-10-31 20:34:44 -0700

    Follow Cards

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,8 +1,10 @@
 class BrandsController < ApplicationController
-	def index
-           @profiles = Profile.all.order(:name).where(:is_brand => true).paginate(:page => params[:page], :per_page =>8)
-	end
 
+
+	def index
+					@profiles = Profile.all.order(:name).where(:is_brand => true).paginate(:page => params[:page], :per_page =>8)
+				
+				end
         def featured
            @profiles = Profile.all.order(:name).where({:is_brand => true , :featured => true}).paginate(:page => params[:page], :per_page =>8)
         end
@@ -12,3 +14,6 @@ class BrandsController < ApplicationController
         end
 
 end
+
+
+
